0c819b348d5c63382ae3dc6c507df80ef9b6044c
Aistė Stikliūtė <aiste@trafi.com>
2021-05-09 19:05:09 +0300

    Add edit address test (as the only enabled addresses test for now)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,10 @@
+Cypress.Commands.overwrite('type', (originalFn, subject, str, options) => { 
+  if (str !== '') {
+    return originalFn(subject, str, options)
+  }
+  return subject
+})
+
 Cypress.Commands.add('login', (username, password) => {
     cy.get('a[class="login"]').click()
     cy.get('#email').type(username)
