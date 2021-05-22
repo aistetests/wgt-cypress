37a81836b460334515ab804a9dd221bd62c35c5b
Aistė Stikliūtė <aiste@trafi.com>
2021-05-22 17:18:43 +0300

    Use login command and user fixture

--- cypress/integration/Addresses.js
+++ cypress/integration/Addresses.js
@@ -1,5 +1,13 @@
 describe('User addresses', () => {
 
+    beforeEach(function () {
+        cy.visit('')
+        cy.fixture('users.json').then((users) => {
+          const user = users[0]
+          cy.login(user.email, user.password)
+        })
+    })  
+
     it('should allow user add address under her account', () => {
       
     })
--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -1,19 +1,19 @@
 describe('Automation Practice Login', () => {
 
     beforeEach(() => {
+      cy.fixture('users.json').as('users')
       cy.visit('')
-      cy.get('a[class="login"]').click()
     })
 
     it('has correct login form title', () => {
+      cy.get('a[class="login"]').click()
       cy.get('h1').should('have.text', 'Authentication')
     })
 
-    it('allows log in with correct username and password', () => {
-      cy.get('#email').type('demo@automate.it')
-      cy.get('#passwd').type('Demo2021')
-      cy.get('#SubmitLogin').click()
-  
+    it('allows log in with correct username and password', function () {
+      const user = this.users[0]
+      
+      cy.login(user.email, user.password)
       cy.url().should('include', 'my-account')
     })
 
