4a9e6bf66e4b730fe3b0f95be8c5ba8fe5f08ec7
Aistė Stikliūtė <aiste@trafi.com>
2021-05-29 17:15:44 +0300

    Preserve user session

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,9 +5,16 @@ Cypress.Commands.overwrite('type', (originalFn, subject, str, options) => {
     return subject
 })
 
+Cypress.Commands.add('preserveAllCookiesOnce', () => {
+    cy.getCookies().then(cookies => {
+      const namesOfCookies = cookies.map(c => c.name)
+      Cypress.Cookies.preserveOnce(...namesOfCookies)
+    })
+})
+
 Cypress.Commands.add('login', (username, password) => {
     cy.get('a[class="login"]').click()
     cy.get('#email').type(username)
     cy.get('#passwd').type(password)
     cy.get('#SubmitLogin').click()
-  })
+})
